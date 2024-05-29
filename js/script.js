c92af627a5a4efe4c2c56d58ef7f29e86bdf2320
Pavel Snigerev <llianellpp.pp@gmail.com>
2024-05-29 19:44:39 +0300

    light/dark theme

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
-document.getElementById('toggle-theme').addEventListener('click', function () {
-    document.body.classList.toggle('dark-theme');
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleButton = document.querySelector('#toggle-theme');
+  const body = document.querySelector('body');
+  
+  function hasClass(elem, className) {
+    return elem.classList.contains(className);
+  }
+  
+  toggleButton.addEventListener('click', () => {
+    if (hasClass(body, 'dark-theme')) {
+      body.classList.remove('dark-theme');
+      body.classList.add('light-theme');
+    } else {
+      body.classList.remove('light-theme');
+      body.classList.add('dark-theme');
+    }
   });
+})
